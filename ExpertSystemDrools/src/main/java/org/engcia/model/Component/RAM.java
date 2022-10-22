69f856cbfdf0765f4f38e02740f2f8efb6a366f3
Vasco Silva <100089965+reiid00@users.noreply.github.com>
2022-10-22 19:16:02 +0100

    Created RAM in Drools

---
+++ ExpertSystemDrools/src/main/java/org/engcia/model/Component/RAM.java
@@ -0,0 +1,20 @@
+package org.engcia.model.Component;
+
+public class RAM extends Component{
+
+    public int speed = 0;
+    public int capacity = 0;
+    public int slotsCount = 0;
+    public int voltage = 0;
+
+    public RAM(){
+        super();
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return super.toString();
+    }
+}
