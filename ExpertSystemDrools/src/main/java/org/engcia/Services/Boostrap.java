568562ca7552a97cf4b251e44d24f17eb54890ed
reiid00 <vascosilva@Vascos-MacBook-Air.local>
2022-10-24 19:40:47 +0100

    bootstrap reading all files

--- ExpertSystemDrools/src/main/java/org/engcia/Services/Boostrap.java
+++ ExpertSystemDrools/src/main/java/org/engcia/Services/Boostrap.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Boostrap {
 
     public static void loadDatabase() throws IOException {
-        //loadGPUs();
+        loadGPUs();
         loadCPUs();
         loadCPUCoolers();
         loadMotherboards();
